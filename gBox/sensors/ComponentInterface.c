85528f02b89fb5683d5efc339eab16e8e69deb57
Romaric Drigon <romaric.drigon@gmail.com>
2012-02-15 11:30:57 +0100

    Rajouté code écrasé

--- gBox/sensors/ComponentInterface.c
+++ gBox/sensors/ComponentInterface.c
@@ -191,7 +191,15 @@ void *ListenSunSpot(void *message1) {
         }
 
         /* température - même fonctionnement */
-        temperature = atoi(strtok(NULL, ";"));
+        int temperature = atoi(strtok(NULL, ";"));
+        
+        /* il faut qu'on applique un coefficient (diviseur)
+         détail du calcul :
+         (scaleMax-scaleMin)/(rangeMax/rangeMin)
+         on remplace avec les valeurs, issues de la datasheet pour la scale et le range est celui d'un int :
+         (165-0)/(255-0) = 0,647058824
+         */
+        temperature = temperature*0.647058824;
 
         if (temperature <= 0xFFFF)
         {
@@ -208,7 +216,7 @@ void *ListenSunSpot(void *message1) {
          détail du calcul :
             (750-0)/(255-0)
          */
-        brightness = brightness/2,94117647;
+        brightness = brightness/2.94117647;
 
         if (brightness <= 0xFFFF)
         {

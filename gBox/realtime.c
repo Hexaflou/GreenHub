4a6d4c40639ef98405aab42e8231c64ee6676f9c
Olivier EBLE <olivier.eble@insa-lyon.fr>
2012-02-20 17:36:54 +0100

    some indentation updates

--- gBox/realtime.c
+++ gBox/realtime.c
@@ -19,77 +19,77 @@ int gRTClose();
 
 /******************** PUBLIC functions *******************************/
 
- 
- /* change la frequence de log du mode temps reel */
- int gRTSetPeriod(int period)
- {
-	 
-	 if( period == 0 && lPeriod != 0 )
-	 {
-		 gRTClose();
-	 }
-	 else if( lPeriod == 0 && period != 0 )
-	 {
-		 gRTInit(period);
-     }
-	 else
-     {
-		 lPeriod=period; 
-     }
-	 
-	 return 0;
- }
- 
- /* initialise la tache de log periodique des donnees*/
- int gRTInit(int period)
- {
-	 lPeriod = period;
-	 return pthread_create( &periodLog, NULL, gRealTimeLog, (void *) NULL);
- }
- 
- /* Kill de la tâche de log periodique des donnees */
- int gRTClose()
- {
-	 pthread_cancel(periodLog);
-	 return 0;
- }
+
+/* change la frequence de log du mode temps reel */
+int gRTSetPeriod(int period)
+{
+
+	if( period == 0 && lPeriod != 0 )
+	{
+		gRTClose();
+	}
+	else if( lPeriod == 0 && period != 0 )
+	{
+		gRTInit(period);
+    }
+	else
+    {
+		lPeriod=period; 
+    }
+
+	return 0;
+}
+
+/* initialise la tache de log periodique des donnees*/
+int gRTInit(int period)
+{
+	lPeriod = period;
+	return pthread_create( &periodLog, NULL, gRealTimeLog, (void *) NULL);
+}
+
+/* Kill de la tâche de log periodique des donnees */
+int gRTClose()
+{
+	pthread_cancel(periodLog);
+	return 0;
+}
 
 /**********************************************************************/
 
 void * gRealTimeLog( void * attr)
 {
 	Sensor* tempSensor = NULL;
-	
+
 	tempSensor=getSensorList();
 	semSensorList = getSemaphore() ;
-	
-	
-	
+
+
+
 	while ( lPeriod != 0 )
 	{
-		
-		
+
+
 		pthread_setcancelstate(PTHREAD_CANCEL_DISABLE,NULL);
-		
+
 		sem_wait(&semSensorList);
-		
+
 		while(tempSensor != NULL)
 		{
 			tempSensor->id[11] = '\0';
 			gCommunicationSendValue(tempSensor->id,tempSensor->value);
 			tempSensor = tempSensor->next;
 		}
-		
-		
+
+
 		sem_post(&semSensorList);
-		
+
 		pthread_setcancelstate(PTHREAD_CANCEL_ENABLE,NULL);
-		
+
 		sleep(lPeriod);
-		
+
 		tempSensor=getSensorList();
 	}
-		
+
 	return (void *) NULL;
 }
 
